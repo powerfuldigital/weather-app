0be149b45959109a8236945f611e55a1cb3c0f96
powerfuldigital <megpower96@hotmail.co.uk>
2021-09-27 08:14:22 +1000

    Added API and JS

--- src/index.js
+++ src/index.js
@@ -1,65 +1,22 @@
-function addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  
-  let now = new Date();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[now.getDay()];
-  let hour = now.getHours();
-  let minute = addZero(now.getMinutes());
-  let currentDate = document.querySelector("#current-day-time");
-  currentDate.innerHTML = `${day} ${hour}:${minute}`;
-  
-  function search(event) {
-    event.preventDefault();
-    let cityElement = document.querySelector("#city");
-    let cityInput = document.querySelector("#find-city-value");
-    cityElement.innerHTML = cityInput.value;
-    let apiKey = "b9ba0314a93083136d968577c718e31d";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  let searchForm = document.querySelector("#find-city");
-  searchForm.addEventListener("submit", search);
-  
-  function displayWeatherCondition(response) {
-    let temperature = Math.round(response.data.main.temp);
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = `${temperature}ºC`;
-  }
-  
-  function displayPositionWeather(weather) {
-    let temp = Math.round(weather.data.main.temp);
-    let weatherDisplay = document.querySelector("#temperature");
-    weatherDisplay.innerHTML = `${temp}`;
-    let newLocation = weather.data.name;
-    let displayLocation = document.querySelector("#city");
-    displayLocation.innerHTML = `${newLocation}`;
-  }
-  
-  function findPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiCoordUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-    axios.get(apiCoordUrl).then(displayPositionWeather);
-  }
-  
-  function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(findPosition);
-  }
-  
-  let currentPosition = document.querySelector("#current-location-button");
-  currentPosition.addEventListener("click", getCurrentPosition);
-  
+function displayTemperature (response){
+let cityElement= document.querySelector ("#city");
+let temperatureELement=document.querySelector ("#temperature");
+let highElement=document.querySelector ("#highTemp");
+let lowElement= document.querySelector ("#lowTemp");
+let windElement = document.querySelector ("#wind");
+let humidityElement = document.querySelector ("#humidity");
+cityElement.innerHTML=(response.data.name);
+temperatureELement.innerHTML= Math.round(response.data.main.temp);
+highElement.innerHTML= Math.round(response.data.main.temp_max);
+lowElement.innerHTML= Math.round(response.data.main.temp_min);
+windElement.innerHTML= Math.round(respone.data.wind.speed);
+humidityElement.innerHTML=(response.data.main.humidity);
+}
+let apiKey ="b9ba0314a93083136d968577c718e31d";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Frankfurt&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature);
+
+
+
+
