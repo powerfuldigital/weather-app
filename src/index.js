fced0b4895f1984425d9772186c27b591e7adb2c
powerfuldigital <megpower96@hotmail.co.uk>
2021-09-29 16:42:12 +1000

    Fixed timestamp issue

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function formatDate (timestamp){
 }
 
 function formatDay(timestamp){
-  let date= new Date(timestamp * 10000);
+  let date= new Date(timestamp * 1000);
   let day= date.getDay();
   let days= ["Sun", "Mon", "Tue","Wed", "Thu", "Fri", "Sat"];
 
